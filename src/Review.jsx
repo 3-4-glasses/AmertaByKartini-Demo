5ed7389b139b8f0a8c21e6c28508a0e546ccc5b3
FaultyDuck <alexanderrk03@gmail.com>
2025-03-27 13:27:19 +0700

    rev 3 again

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,13 +1,22 @@
-import './Revision.css'
-
-function App() {
+function Review() {
 
     return (
+<<<<<<< HEAD:src/Revision.jsx
       <div className="flex flex-wrap bg-blue-200 pt-[20vh] min-h-screen justify-center">
         <div className="container flex flex-wrap justify-center items-center max-w-[60vw] mx-auto">
           <div className="flex w-full max-w-full max-h-[8vh]">
             <div className="flex-1 p-2">
               <img src="face.png" alt="Orr or urr" className="rounded-full h-[15vh] w-[15vh] bg-gray-100 object-cover mx-auto" />
+=======
+        <div className="flex flex-wrap bg-yellow-100 pt-20 pb-20">
+          <div className="container mx-auto flex flex-col items-center justify-center bg-orange-50 shadow-lg w-1/2 p-4">
+            <div className="w-full h-[5vh] text-4xl text-center p-2">I love this Product!</div>
+    
+            <div className="w-full p-10">
+              <p className="text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+>>>>>>> f1def533096adbd2e48bff5b2c856fcbb9d173d5:src/Review.jsx
             </div>
             <div className="flex-1 p-2">
               <img src="face.png" alt="Orr or urr" className="rounded-full h-[15vh] w-[15vh] bg-gray-100 object-cover mx-auto" />
@@ -36,4 +45,4 @@ function App() {
     )
   }
   
-  export default App
+  export default Review
