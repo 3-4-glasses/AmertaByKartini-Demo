e1a7f97450468bafa2ff3ecea307d55857e1ec8a
osten-antonio <ostenantonio1@gmail.com>
2025-03-27 15:18:37 +0700

    revision

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,47 +1,53 @@
-function Review() {
+import pfp from "./assets/face.png"
 
+function PfpImage(props) {
+  return (
+    <div className="absolute -top-[5vh] left-1/2 transform -translate-x-1/2 z-10">
+      <img src={props.pfp} className="rounded-full w-24 h-24 object-cover aspect-square border-4 border-white shadow-lg" alt="Orr or urr" />
+    </div>
+  );
+}
+
+
+function ReviewBody(props){
+  return(
+  <div className="relative flex flex-col max-w-[90vw] sm:max-w-[30vw] bg-[#F6F5F0] p-4 text-center  place-items-center w-[50%] m-[2vh] mt-0 ">
+    <PfpImage pfp={pfp} />
+
+    <div className="w-full text-2xl text-center pb-2 mt-[10vh]">
+      <p>{props.heading}</p>
+    </div>
+    <div className="w-full p-2 text-center text-base">
+      <p>
+        {props.content}        
+      </p>
+    </div>
+    <div className="w-full p-2 text-center text-lg">{props.name}</div>
+  </div>
+  );
+}
+
+function Review() {
     return (
-<<<<<<< HEAD:src/Revision.jsx
-      <div className="flex flex-wrap bg-blue-200 pt-[20vh] min-h-screen justify-center">
-        <div className="container flex flex-wrap justify-center items-center max-w-[60vw] mx-auto">
-          <div className="flex w-full max-w-full max-h-[8vh]">
-            <div className="flex-1 p-2">
-              <img src="face.png" alt="Orr or urr" className="rounded-full h-[15vh] w-[15vh] bg-gray-100 object-cover mx-auto" />
-=======
-        <div className="flex flex-wrap bg-yellow-100 pt-20 pb-20">
-          <div className="container mx-auto flex flex-col items-center justify-center bg-orange-50 shadow-lg w-1/2 p-4">
-            <div className="w-full h-[5vh] text-4xl text-center p-2">I love this Product!</div>
-    
-            <div className="w-full p-10">
-              <p className="text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
->>>>>>> f1def533096adbd2e48bff5b2c856fcbb9d173d5:src/Review.jsx
-            </div>
-            <div className="flex-1 p-2">
-              <img src="face.png" alt="Orr or urr" className="rounded-full h-[15vh] w-[15vh] bg-gray-100 object-cover mx-auto" />
-            </div>
-            <div className="flex-1 p-2">
-              <img src="face.png" alt="Orr or urr" className="rounded-full h-[15vh] w-[15vh] bg-gray-100 object-cover mx-auto" />
-            </div>
-          </div>
-          <div className="flex flex-wrap z-0">
-            {[...Array(3)].map((_, index) => (
-              <div key={index} className="flex-1 max-w-[20vw] bg-gray-100 m-2 p-4 text-center">
-                <div className="w-full text-4xl text-center pb-2 mt-[10vh]">
-                  <p>I love this Product!</p>
-                </div>
-                <div className="w-full p-2 text-center text-lg">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </p>
-                </div>
-                <div className="w-full p-2 text-center text-lg">David Nielson</div>
-              </div>
-            ))}
-          </div>
+        <div className="flex flex-col sm:flex-row flex-wrap justify-center place-items-center bg-[#BCD0DB] pt-20 pb-20">
+              <ReviewBody 
+                heading="I love this Product!"
+                content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eligendi, iure vero nihil expedita voluptatem iusto deleniti at, saepe nesciunt dolorem voluptate unde molestiae atque. Libero natus explicabo maxime perspiciatis?"
+                name="David Nielson"
+              />
+              
+              <ReviewBody 
+                heading="I love this Product!"
+                content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eligendi, iure vero nihil expedita voluptatem iusto deleniti at, saepe nesciunt dolorem voluptate unde molestiae atque. Libero natus explicabo maxime perspiciatis?"
+                name="David Nielson"
+              />
+
+              <ReviewBody 
+                heading="I love this Product!"
+                content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eligendi, iure vero nihil expedita voluptatem iusto deleniti at, saepe nesciunt dolorem voluptate unde molestiae atque. Libero natus explicabo maxime perspiciatis?"
+                name="David Nielson"
+              />
         </div>
-      </div>
     )
   }
   
